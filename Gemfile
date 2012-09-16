8f1a267209ed9af8d7fb7c26fad1e829949cfa2f
Neil Pullman <neil@descend.org>
2012-09-16 02:38:34 -0400

    upgrade to mongoid3

--- Gemfile
+++ Gemfile
@@ -1,21 +1,23 @@
 source "http://rubygems.org"
 gem "rack"
-gem 'rack-flash', :git => 'https://github.com/ChrisCauser/rack-flash.git'
+gem 'rack-flash', :git => 'https://github.com/chy-causer/rack-flash'
 gem 'rake'
 gem "thin"
 gem "sinatra"
-gem "bson", "1.5.2"
-gem "bson_ext", "1.5.2"
+gem "bson"
+gem "bson_ext"
 gem "mongoid"
 gem 'mongoid_taggable_with_context', :git => 'https://github.com/neiltron/mongoid_taggable_with_context.git'
 gem "haml"
 gem 'json'
 gem "metaid"
 gem 'grape', :git => 'https://github.com/intridea/grape.git', :branch => 'frontier'
+gem 'garner'
 gem 'warden'
 gem 'mongoid_token'
 gem 'boxer'
 gem 'heroku'
+gem 'pony'
 
 group :test do
 	gem 'rack-test', :git => 'https://github.com/brynary/rack-test.git'
--- models/user.rb
+++ models/user.rb
@@ -1,30 +1,33 @@
 require 'mongoid'
 require 'boxer'
+require 'pony'
 
 class User
 	include Mongoid::Document
 	include Mongoid::Timestamps
 	include Mongoid::Paranoia
-	
+
 	field :name, :type => String
   field :email, :type => String
   field :password_hash, :type => String
   field :confirmed, :type => Boolean
   field :confirm_token, :type => String
 	field :last_update, :type => String
+  field :items_updated_at, :type => DateTime
   field :password_salt, :type => String
-	
-	index :email, :unique => true
-	
+
+	index({ :email => 1 }, { :unique => true })
+
 	has_one :accesskey, :class_name => 'AccessKey'
 	has_many :items
-	
+
   before_create :process_on_create
-  
+  after_save :encrypt_pass
+
   def authenticated? (password)
     self.password_hash == encrypt(password)
   end
-  
+
   def self.authenticate (email = nil, password = nil, accesskey = nil)
     user = User.where( :email => email.downcase ).first
     user = nil unless user.nil? || user.authenticated?(password)
@@ -37,10 +40,10 @@ class User
       else
         user = key.user
       end
-    else 
+    else
       user.remembered_pass! if !user.confirm_token.nil? #clear confirm token if user has logged in
     end
-    
+
     #give user an access key if they don't have one. this should only really happen on first login
     if !user.nil? && user.accesskey.nil?
       user.accesskey = AccessKey.new
@@ -55,18 +58,28 @@ class User
 
   def send_confirmation
     subject = 'Signup confirmation'
-    body = "You recently signed up for DOCTOTHORPEM. <br><br><a href='http://doctothorp.em/confirm?key=" + self.confirm_token + "'>Click here to confirm your account</a>."
-    
-    Pony.mail :to => self.email,
+    body = "You recently signed up for Math. <br><br><a href='http://doctothorp.em/confirm?key=" + self.confirm_token + "'>Click here to confirm your account</a>."
+
+    Pony.mail :to => email,
               :from => 'no-reply@doctothorp.em',
               :subject => subject,
               :body => body,
               :headers => { 'Content-Type' => 'text/html' },
+              :via => :smtp,
               :via_options => PONY_CONFIG
   end
-  
+
   def send_forgot_pass_email
-    Resque.enqueue(WorkHQ::ForgotPassEmail, self.id.to_s)
+    subject = 'Forgot password'
+    body = "Someone used the forgot password form at Math. <br><br><a href='http://doctothorp.em/resetpw?key=" + self.confirm_token + "'>Click here to change your password</a>."
+
+    Pony.mail :to => email,
+              :from => 'neil@descend.org',
+              :subject => subject,
+              :body => body,
+              :headers => { 'Content-Type' => 'text/html' },
+              :via => :smtp,
+              :via_options => PONY_CONFIG
   end
 
   def confirm!
@@ -84,9 +97,18 @@ class User
     self.save
   end
 
+  def encrypt_pass
+    unless self.password.nil?
+      self.password_hash = encrypt(self.password)
+      self.password = nil
+
+      self.save
+    end
+  end
+
   def process_on_create
-    self.password_hash = encrypt(self.password)
-    self.password = nil
+    encrypt_pass
+
     self.confirm_token = new_token
   end
 
