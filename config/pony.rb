756ba23c89aa90c12edbbf7613145c455ee7d964
Neil Pullman <neil@descend.org>
2012-09-16 02:42:31 -0400

    add pony config

---
+++ config/pony.rb
@@ -0,0 +1,9 @@
+PONY_CONFIG = {
+  :address              => 'smtp.gmail.com',
+  :port                 => '587',
+  :enable_starttls_auto => true,
+  :user_name            => 'neil@descend.org',
+  :password             => 'breakneck',
+  :authentication       => :plain,
+  :domain               => "localhost.localdomain"
+}
